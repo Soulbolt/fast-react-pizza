faceff5bccf8c4ab906db2985742a66744047347
Soulbolt <soulbolt@gmail.com>
2024-02-15 10:08:31 -0700

    Add conditional if t here is no query do nothing, Add navigate variable to append to route /order/query and set query back to null.

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
   const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(query);
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
   }
 
   return (
