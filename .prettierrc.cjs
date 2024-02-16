b62ff7096ac798ca8c4e9d8b69fa24438c0de9b2
Soulbolt <soulbolt@gmail.com>
2024-02-16 08:50:32 -0700

    Added Roboto Mono moonscape font to the index.html. Fix issue with prettier not working with tailwindcss.

--- .prettierrc.cjs
+++
@@ -1,3 +0,0 @@
-export default {
-  plugins: [required("prettier-plugin-tailwindcss")],
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
     extend: {},
   },
   plugins: [],
